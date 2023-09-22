f96a7a7db7fb6dc524f233506e2e51c6dca3143c
Logan Grosz <logan.grosz@b9c.com>
2023-09-22 15:40:45 -0600

    Pipeline structure

--- main.cpp
+++ main.cpp
@@ -11,14 +11,20 @@
  * should be provided.
  */
 template <typename T>
-class Producer
+class Producer : public QThread
 {
 
 public:
-    explicit Producer(int writer, QObject *parent = nullptr)
-        : writer(writer)
+    explicit Producer(QObject *parent = nullptr)
+        : QThread(parent), fd(0), ok(false)
     {
 
+    }
+
+    void to(int fd)
+    {
+        ok = true;
+        this->fd = fd;
     }
 
     /**
@@ -26,7 +32,10 @@ public:
      */
     void produce(T t)
     {
-        write(writer, &t, sizeof(T));
+        if (ok) {
+            qDebug() << "produce" << fd;
+            write(fd, &t, sizeof(T));
+        }
     }
 
     /**
@@ -34,39 +43,15 @@ public:
      */
     void end()
     {
-        close(writer);
-    }
-
-private:
-    int writer;
-
-};
-
-/**
- * A thread which writes `n` random integers to `fd`.
- */
-class RandomUIntProducer : public QThread
-{
-
-public:
-    explicit RandomUIntProducer(unsigned int n, int fd, QObject *parent = nullptr)
-        : n(n), producer(fd)
-    {
-
-    }
-
-    void run() override
-    {
-        for (int i = 0; i < n; ++i) {
-            producer.produce(QRandomGenerator::global()->bounded(10));
+        if (ok) {
+            close(fd);
         }
-
-        producer.end();
     }
 
 private:
-    const unsigned int n;
-    Producer<quint32> producer;
+    bool ok;
+    int fd;
+
 };
 
 /*
@@ -75,37 +60,107 @@ private:
  * should be provided.
  */
 template <typename T>
-class Consumer
+class Consumer : public QThread
 {
 
 public:
-    explicit Consumer(int reader, QObject *parent = nullptr)
-        : reader(reader)
+    explicit Consumer(QObject *parent = nullptr)
+        : QThread(parent), ok(false)
     {
 
+    }
+
+    void from(int fd)
+    {
+        ok = true;
+        this->fd = fd;
     }
 
     ~Consumer()
     {
-        close(reader);
+        close(fd);
     }
 
     bool consume(T *t)
     {
-        return read(reader, t, sizeof(T)) > 0;
+        qDebug() << "consume" << fd;
+        return read(fd, t, sizeof(T)) > 0;
     }
 
 private:
-    int reader;
+    bool ok;
+    int fd;
 
 };
 
 template <typename T>
-class Logger : public QThread
+class Pipeline
+{
+
+public:
+    // I'm not a fan of having the producer and consumer inside the pipeline?
+    // Should those be external and this class should provide methods for
+    // writing and reading?
+    explicit Pipeline(Producer<T> &initial, Consumer<T> &final)
+        : initial(initial), final(final)
+    {
+        int fd[2];
+        if (pipe(fd) == -1) {
+            perror("Failed to create pipe");
+        }
+
+        initial.to(fd[1]);
+        final.from(fd[0]);
+    }
+
+    void run()
+    {
+        initial.start();
+        final.start();
+
+        initial.wait();
+        final.wait();
+    }
+
+private:
+    Producer<T> &initial;
+    Consumer<T> &final;
+
+};
+
+/**
+ * A thread which writes `n` random integers to `fd`.
+ */
+class RandomUIntProducer : public Producer<int>
+{
+
+public:
+    explicit RandomUIntProducer(unsigned int n, QObject *parent = nullptr)
+        : Producer(parent), n(n)
+    {
+
+    }
+
+    void run() override
+    {
+        for (int i = 0; i < n; ++i) {
+            produce(QRandomGenerator::global()->bounded(10));
+        }
+
+        end();
+    }
+
+private:
+    const unsigned int n;
+
+};
+
+template <typename T>
+class Logger : public Consumer<T>
 {
 public:
-    explicit Logger(int reader, QObject *parent = nullptr)
-        : QThread(parent), consumer(reader)
+    explicit Logger(QObject *parent = nullptr)
+        : Consumer<T>(parent)
     {
 
     }
@@ -122,109 +177,14 @@ public:
 
 private:
     Consumer<T> consumer;
-
-};
-
-template <typename T>
-class LogAndPassItOn : public QThread
-{
-
-public:
-    explicit LogAndPassItOn(int reader, int writer, QObject *parent = nullptr)
-        : QThread(parent), consumer(reader), producer(writer)
-    {
-
-    }
-
-    void run() override
-    {
-        T t;
-
-        int count = 0;
-        while (consumer.consume(&t)) {
-            qDebug() << ++count << t;
-            producer.produce(t);
-        }
-
-        producer.end();
-    }
-
-private:
-    Consumer<T> consumer;
-    Producer<T> producer;
-
-};
-
-template <typename T>
-class Doubler : public QThread
-{
-
-public:
-    explicit Doubler(int reader, int writer, QObject *parent = nullptr)
-        : QThread(parent), consumer(reader), producer(writer)
-    {
-
-    }
-
-    void run() override
-    {
-        T t;
-
-        int count = 0;
-        while (consumer.consume(&t)) {
-            producer.produce(t * 2);
-        }
-
-        producer.end();
-    }
-
-private:
-    Consumer<T> consumer;
-    Producer<T> producer;
 
 };
 
 int main(void)
 {
-    // TODO How do I hide this? Could I have some sort of class for the parts
-    // of the pipeline that need methods like `setIn` and `setOut`? Then the
-    // QThread idea could be private, `setIn`/`setOut` can invalidate the
-    // class, and if they're appropriately set the thread gets started. Will
-    // this cause data to be missed? I think I can then hide the pipe creation
-    // in another class which contains the in and out implementations.
-
-    int fd1[2];
-    if (pipe(fd1) == -1) {
-        perror("Could not create pipe");
-        return 1;
-    }
-
-    int fd2[2];
-    if (pipe(fd2) == -1) {
-        perror("Could not create pipe");
-        return 1;
-    }
-
-    int fd3[2];
-    if (pipe(fd3) == -1) {
-        perror("Could not create pipe");
-        return 1;
-    }
-
-    RandomUIntProducer start(10, fd1[1]);
-    LogAndPassItOn<quint32> logger(fd1[0], fd2[1]);
-    Doubler<quint32> doubler(fd2[0], fd3[1]);
-    Logger<quint32> end(fd3[0]);
-
-    start.start();
-    logger.start();
-    doubler.start();
-    end.start();
-
-    start.wait();
-    logger.wait();
-    doubler.wait();
-    end.wait();
-
+    RandomUIntProducer producer(10);
+    Logger<int> consumer;
+    Pipeline<int> pipeline(producer, consumer);
+    pipeline.run();
     return 0;
 }
