634cf1d556d9ab2566578e99910d8006cd04beae
Logan Grosz <logan.grosz@b9c.com>
2023-09-26 14:53:15 -0600

    Example pipeline elements

--- main.cpp
+++ main.cpp
@@ -3,6 +3,28 @@
 #include <QRandomGenerator>
 #include "pipeline.h"
 
+class PipelineDoubler : public PipelineElement
+{
+
+public:
+    void input(unsigned char byte) override
+    {
+        emit output(byte * 2);
+    }
+
+};
+
+class PipelinePlusOner : public PipelineElement
+{
+
+public:
+    void input(unsigned char byte) override
+    {
+        emit output(byte + 1);
+    }
+
+};
+
 int main(int argc, char **argv)
 {
     QCoreApplication a(argc, argv);
@@ -10,11 +32,13 @@ int main(int argc, char **argv)
     PipelineElement e1;
     PipelineElement e2;
     PipelineElement e3;
+    PipelinePlusOner e4;
+    PipelineDoubler e5;
 
-    Pipeline pipeline { &e1, &e2, &e3 };
+    Pipeline pipeline { &e1, &e2, &e3, &e4, &e5 };
 
     for (int i = 0; i < 10; i++) {
-        unsigned char c = 'A' + (QRandomGenerator::global()->bounded(26));
+        unsigned char c = QRandomGenerator::global()->bounded(10);
         qDebug() << "Inputting" << c;
         pipeline.input(c);
     }
